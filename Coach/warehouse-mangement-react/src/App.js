7bd797cb528ad03bd5a5b8d43668084cfffa111c
ntp945812 <ntp945812@hotmail.com>
2019-12-30 21:32:32 +0800

    Make some change.

--- Coach/warehouse-mangement-react/src/App.js
+++ Coach/warehouse-mangement-react/src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import LoginView from "./LoginView.js";
 
 function App() {
-  return (
-    <div>
-      <LoginView className="LoginView" />
-    </div>
-  );
+	const [isUserLogin, setIsUserLogin] = useState(false);
+	let element;
+	if (isUserLogin) {
+		element = (
+			<div className="mainView">
+				<h1>登入成功</h1>
+			</div>
+		);
+	} else {
+		element = (
+			<div className="LoginView">
+				<LoginView setIsUserLogin={setIsUserLogin} />
+			</div>
+		);
+	}
+	return element;
 }
 
 export default App;
--- Coach/warehouse-mangement-react/src/LoginView.js
+++ Coach/warehouse-mangement-react/src/LoginView.js
@@ -1,24 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import react_logo from "./react-logo.png";
 import "./LoginView.css";
 
 function LoginView(props) {
-	return (
-		<div align="center">
-			<img src={react_logo} alt="React Logo" width="20%" height="auto" />
-			<form action="" className="LoginView">
-				<label htmlFor="userName">
-					帳號
-					<input type="text" className="Input" />
-				</label>
-				<br />
-				<label htmlFor="password">
-					密碼
-					<input type="password" className="Input" />
-				</label>
-			</form>
+	const setUserLogin = props.setUserLogin;
+	const [userName, setUserName] = useState("");
+	const [password, setPassword] = useState("");
+	let element;
+
+	element = (
+		<div className="wrapper">
+			<div className="header">
+				<div className="logo">
+					<img src={react_logo} alt="React Logo" />
+				</div>
+				<div className="form">
+					<div className="input_field">
+						<input
+							type="text"
+							id=""
+							className="input"
+							placeholder="Username"
+						/>
+					</div>
+					<div className="input_field">
+						<input
+							type="password"
+							className="input"
+							placeholder="Password"
+						/>
+					</div>
+					<div className="btn">
+						<button>Log In</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
+
+	return element;
 }
 
 export default LoginView;
