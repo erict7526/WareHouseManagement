4d196a4376242abda8b288f18cbdb30db833c237
ntp945812 <ntp945812@hotmail.com>
2020-01-10 21:43:01 +0800

    Set up a customized table.

--- Coach/warehouse-mangement-react/src/PickUpView.js
+++ Coach/warehouse-mangement-react/src/PickUpView.js
@@ -2,6 +2,29 @@ import React, { useState } from "react";
 import "./PickUpView.css";
 import react_logo from "./react-logo.png";
 
+const testData = [
+	{
+		number: "1269-6513",
+		name: "test1",
+		specification: "test1"
+	},
+	{
+		number: "1235-4546",
+		name: "test2",
+		specification: "test2"
+	},
+	{
+		number: "6549-6323",
+		name: "test3",
+		specification: "test3"
+	},
+	{
+		number: "66854-11321",
+		name: "test4",
+		specification: "test4"
+	}
+];
+
 function PickUpView(props) {
 	let element;
 
@@ -14,18 +37,7 @@ function PickUpView(props) {
 				</div>
 
 				<div className="search-result">
-					<table>
-						<tr>
-							<th>編號</th>
-							<th>名稱</th>
-							<th>規格</th>
-						</tr>
-						<tr>
-							<td>1303-1621</td>
-							<td>測試</td>
-							<td>測試</td>
-						</tr>
-					</table>
+					<Table data={testData} />
 				</div>
 			</div>
 		</div>
@@ -35,3 +47,39 @@ function PickUpView(props) {
 }
 
 export default PickUpView;
+
+function Table(props) {
+	let element;
+
+	element = (
+		<table>
+			<tr>
+				<th>編號</th>
+				<th>名稱</th>
+				<th>規格</th>
+			</tr>
+			{props.data.map(d => (
+				<DataTr data={d} />
+			))}
+		</table>
+	);
+
+	return element;
+}
+
+function DataTr(props) {
+	let element;
+	const data = props.data;
+	element = (
+		<tr>
+			<td>{data["number"]}</td>
+			<td>{data["name"]}</td>
+			<td>{data["specification"]}</td>
+			<td>
+				<input type="text" />
+			</td>
+		</tr>
+	);
+
+	return element;
+}
