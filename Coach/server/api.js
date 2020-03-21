b8e30c028b309c779d9bf654cf0cb835d1f2e40b
ntp945812 <ntp945812@hotmail.com>
2020-03-21 22:43:14 +0800

    Put plugins in api.js.

--- Coach/server/api.js
+++ Coach/server/api.js
@@ -3,8 +3,13 @@ const { makeExtendSchemaPlugin, gql } = require("graphile-utils");
 exports.randomNumber = makeExtendSchemaPlugin(build => {
 	return {
 		typeDefs: gql`
+			type UserTest {
+				name: String
+				num: Int
+			}
 			extend type Query {
 				rn: Float
+				me: UserTest
 			}
 		`,
 		resolvers: {
@@ -19,6 +24,14 @@ exports.randomNumber = makeExtendSchemaPlugin(build => {
 					} catch (e) {
 						throw e;
 					}
+				},
+				me: async (_query, args, content, resolveInfo) => {
+					const { pgClient } = content;
+					try {
+						return { name: "Coach" };
+					} catch (e) {
+						throw e;
+					}
 				}
 			}
 		}
@@ -29,16 +42,17 @@ exports.searchByAPI = makeExtendSchemaPlugin(build => {
 	return {
 		typeDefs: gql`
 			extend type Query {
-				searchByAPI(search: String): [LastDetail]!
+				searchByAPI(search: String, limit: Int = 0): [LastDetail]!
 			}
 		`,
 		resolvers: {
 			Query: {
 				searchByAPI: async (_query, args, content, resolveInfo) => {
 					const { pgClient } = content;
+					const limit = args.limit <= 0 ? "All" : args.limit;
 					try {
 						const res = await pgClient.query(
-							`select * from last_detail l_d where l_d.id ilike ('%' || $1 || '%') or l_d.name ilike ('%' || $1 || '%') or l_d.spec ilike ('%' || $1 || '%');`,
+							`select * from last_detail l_d where l_d.id ilike ('%' || $1 || '%') or l_d.name ilike ('%' || $1 || '%') or l_d.spec ilike ('%' || $1 || '%') limit ${limit};`,
 							[args.search]
 						);
 						return res.rows;
